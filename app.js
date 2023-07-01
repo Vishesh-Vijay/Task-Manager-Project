34c5de7e60e6ad89e9977d633f7e5859180c3d01
Vishesh Vijayvargiya <vijay.vishu03@gmail.com>
2023-07-01 22:18:25 +0530

    changes

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ const port= process.env.PORT || 8000
 
 const start=async()=>{
     try{
-        await connectDB(process.env.MONGO_URI)
+        await connectDB(
+          "mongodb+srv://Vishesh:1234@cluster0.wjraimd.mongodb.net/Task-Manager?retryWrites=true&w=majority"
+        );
         app.listen(port,()=>console.log('Server is listening on port '+port+'...'))
     }catch(err){
         console.log(err)
