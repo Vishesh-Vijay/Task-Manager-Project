9859997af3690076f7be1be0736a3b9e394f45a8
Vishesh Vijayvargiya <vijay.vishu03@gmail.com>
2023-07-01 21:52:40 +0530

    issue resolved

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,4 +1,4 @@
-const Tasks= require('./models/Task')
+const Tasks= require('./models/task')
 const asyncWrapper=require('../middleware/async')
 const {createCustomError}=require('../errors/custom-error')
 const getAllTasks=asyncWrapper(async(req,res)=>{
